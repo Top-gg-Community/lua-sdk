d2a0176c347423f03365f7d25258d64c567d0b80
matthewthechickenman <65732060+matthewthechickenman@users.noreply.github.com>
2021-02-23 14:27:46 +0000

    add type protection

--- topgg.lua
+++ topgg.lua
@@ -5,11 +5,17 @@ topgg.Autoposter = { _r = false };
 topgg.Webhook = { _r = false };
 
 function topgg.Api:init (token)
+    if type(token) ~= "string" then
+        error("[topgg-lua API] Token must be a string")
+    end;
     self._r = true;
     self.token = token;
 end;
 
 function topgg.Autoposter:init (token)
+    if type(token) ~= "string" then
+        error("[topgg-lua API] Token must be a string")
+    end;
     self._r = true;
     self.token = token;
 end;
