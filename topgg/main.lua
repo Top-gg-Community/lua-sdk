ea30921509503e7c75f9c65ade72e456b108bb0e
Voltrex <62040526+VoltrexMaster@users.noreply.github.com>
2021-02-23 21:29:52 +0330

    Fix require paths
    Pain.

--- topgg/main.lua
+++ topgg/main.lua
@@ -1,7 +1,7 @@
 local topgg = {};
 
-topgg.Autoposter = require('modules/autoposter');
-topgg.Api = require('modules/api');
+topgg.Autoposter = require('topgg/modules/autoposter');
+topgg.Api = require('topgg/modules/api');
 
 return topgg;
 
