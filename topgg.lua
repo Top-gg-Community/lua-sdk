5141888751641c0d18e2f92a2c5f7b1071aa1cdd
matthewthechickenman <65732060+matthewthechickenman@users.noreply.github.com>
2021-02-23 14:23:54 +0000

    lint my broken ass code

--- topgg.lua
+++ topgg.lua
@@ -1,17 +1,19 @@
 local topgg = {}
 
-topgg.Api = { _r: false }
-topgg.Autoposter { _r: false }
-topgg.Webhook = { _r: false }
+topgg.Api = { _r = false };
+topgg.Autoposter = { _r = false };
+topgg.Webhook = { _r = false };
 
-function topgg.Api.init (token) {
-    self._r = true
-    self.token = token
-}
-function topgg.Autoposter.init (token) {
-    self._r = true
-    self.token = token
-}
+function topgg.Api:init (token)
+    self._r = true;
+    self.token = token;
+end;
+
+function topgg.Autoposter:init (token)
+    self._r = true;
+    self.token = token;
+end;
+
 
 -- Webhooks need to be init'd differently (with a HTTP server, to listen for top.ggs http requests, duh!).
 
