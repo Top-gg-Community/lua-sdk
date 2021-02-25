974211fcc8f05cf04a170a391761c9c672453b3d
matthewthechickenman <65732060+matthewthechickenman@users.noreply.github.com>
2021-02-25 15:30:43 +0000

    add autoposter base

--- topgg/init.lua
+++ topgg/init.lua
@@ -1,5 +1,6 @@
 package.path = './deps/?/init.lua;./deps/?.lua;./topgg/lib/?.lua;./deps/secure-socket/?.lua;' .. package.path;
 return {
     Api = require('api'),
+    Autoposter = require('autoposter'),
     test = require('test')
 }
---
+++ topgg/lib/autoposter.lua
@@ -0,0 +1,3 @@
+local Autoposter = {__time = 1800000}
+
+return Autoposter
