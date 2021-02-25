43734406e415959d87d1b6af23dc3ed017b1b5bb
Voltrex <62040526+VoltrexMaster@users.noreply.github.com>
2021-02-25 19:30:55 +0330

    Implement AutoPoster
    Implemented AutoPoster for the lib.

--- topgg/lib/autoposter.lua
+++ topgg/lib/autoposter.lua
@@ -1,3 +1,26 @@
-local Autoposter = {__time = 1800000}
+local timer = require('timer');
+local setInterval = timer.setInterval;
+local Api = require('api');
+local EventEmitter = require('EventEmitter');
 
-return Autoposter
+local AutoPoster = require('class')('AutoPoster', EventEmitter);
+
+function AutoPoster:init(client)
+  if not client or not client._user or not client._user.id or not client._token then
+    error("argument 'client' must be a client instance");
+  end
+
+  self.__post = Api:init(client._token, client._user.id):postStats;
+
+  setInterval(function()
+    local poster = coroutine.create(function()
+    self.__post({serverCount = client._guilds.__len(), shardCount = client._shard_count});
+    self:emit('posted');
+  end);
+    coroutine.resume(poster);
+  end, 900000);
+
+  return self;
+end
+
+return AutoPoster;
