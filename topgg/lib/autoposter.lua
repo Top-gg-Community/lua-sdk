452b299708c3d6c0c0f824b6684a0bce9c27213e
matthewthechickenman <65732060+matthewthechickenman@users.noreply.github.com>
2021-02-25 16:44:43 +0000

    Update autoposter.lua

--- topgg/lib/autoposter.lua
+++ topgg/lib/autoposter.lua
@@ -6,8 +6,8 @@ local EventEmitter = require('EventEmitter');
 local AutoPoster = require('class')('AutoPoster', EventEmitter);
 
 function AutoPoster:init(apiToken, client)
-  if not client or not client.user or not client.user.id or not client.guilds  then
-    error("argument 'client' must be a discordia client instance");
+  if not client or not client.user or not client.user.id or not client.guilds or not client.guilds.__len() then
+    error("argument 'client' must be a discordia/discordia-like client instance");
   end
 
   Api:init(apiToken, client.user.id)
